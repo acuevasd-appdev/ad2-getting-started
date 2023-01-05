7d91bfa3fed2c5ceb27d6eb934c1e9c2a054520d
acuevasd <acuevasd@chicagobooth.edu>
2023-01-05 21:22:56 +0000

    appdev2_completed

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,18 @@
 class MoviesController < ApplicationController
+  def new
+    @the_movie = Movie.new
+    render template: "movies/new.html.erb"
+
+
+  end
+
+  def edit
+    the_id = params.fetch("id")
+    @the_movie = Movie.where({ :id => the_id }).first
+
+    render template: "movies/edit.html.erb"
+  end
+
   def index
     matching_movies = Movie.all
 
@@ -18,21 +32,27 @@ class MoviesController < ApplicationController
   end
 
   def create
-    the_movie = Movie.new
-    the_movie.title = params.fetch("query_title")
-    the_movie.description = params.fetch("query_description")
-    the_movie.released = params.fetch("query_released", false)
+    @the_movie = Movie.new
+    @the_movie.title = params.fetch("query_title")
+    @the_movie.description = params.fetch("query_description")
+    @the_movie.released = params.fetch("query_released", false)
 
-    if the_movie.valid?
-      the_movie.save
+    if @the_movie.valid?
+      @the_movie.save
       redirect_to("/movies", { :notice => "Movie created successfully." })
     else
-      redirect_to("/movies", { :alert => the_movie.errors.full_messages.to_sentence })
+      cookies[:title] = params.fetch("query_title")
+      cookies[:description] = params.fetch("query_description")
+      #cookies[:released] = params.fetch("query_released", false)
+
+      #redirect_to("/movies/new", { :alert => the_movie.errors.full_messages.to_sentence })
+      render template: "movies/new.html.erb"
+
     end
   end
 
   def update
-    the_id = params.fetch("id")
+    @the_id = params.fetch("id")
     the_movie = Movie.where({ :id => the_id }).first
 
     the_movie.title = params.fetch("query_title")
@@ -41,9 +61,13 @@ class MoviesController < ApplicationController
 
     if the_movie.valid?
       the_movie.save
-      redirect_to("/movies/#{the_movie.id}", { :notice => "Movie updated successfully."} )
+      redirect_to("/movies/%=@the_id%>", { :notice => "Movie updated successfully."} )
     else
-      redirect_to("/movies/#{the_movie.id}", { :alert => the_movie.errors.full_messages.to_sentence })
+      cookies[:title] = params.fetch("query_title")
+      cookies[:description] = params.fetch("query_description")
+      cookies[:released] = params.fetch("query_released", false)
+
+      redirect_to("/movies/%=@the_id%/edit", { :alert => the_movie.errors.full_messages.to_sentence })
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Rails.application.routes.draw do
+  resources :books
   # Routes for the Movie resource:
 
   # CREATE
-  post("/insert_movie", { :controller => "movies", :action => "create" })
+  post("/movies", { :controller => "movies", :action => "create" })
           
   # READ
   get("/movies", { :controller => "movies", :action => "index" })
   
+  get("/movies/new", { :controller => "movies", :action => "new" })
+
   get("/movies/:id", { :controller => "movies", :action => "show" })
-  
+
   # UPDATE
   
+  get("movies/:id/edit", { :controller => "movies", :action => "edit" })
+
   patch("movies/:id", { :controller => "movies", :action => "update" })
   
   # DELETE
