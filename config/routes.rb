9d4de472c7db6d8c8d4542d767acf54d59e880d8
acuevasd <acuevasd@chicagobooth.edu>
2023-01-05 20:02:24 +0000

    appdev2_part1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  # Routes for the Movie resource:
+
+  # CREATE
+  post("/insert_movie", { :controller => "movies", :action => "create" })
+          
+  # READ
+  get("/movies", { :controller => "movies", :action => "index" })
+  
+  get("/movies/:id", { :controller => "movies", :action => "show" })
+  
+  # UPDATE
+  
+  patch("movies/:id", { :controller => "movies", :action => "update" })
+  
+  # DELETE
+  delete("/movies/:id", { :controller => "movies", :action => "destroy" })
+
+  #------------------------------
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
